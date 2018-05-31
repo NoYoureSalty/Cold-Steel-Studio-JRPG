ca0cac9694284c47dd1779525433a43885154b15
Rory <mcfarlandrory99@gmail.com>
2018-05-30 23:05:07 -0700

    Added Graphics POC

---
+++ JRPG/Assets/Scripts/PlayerMove.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMove : MonoBehaviour {
+    public float speed = 0;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        transform.Translate(new Vector3(Input.GetAxis("Horizontal"),0.0f,Input.GetAxis("Vertical"))*speed*Time.deltaTime);
+	}
+}
