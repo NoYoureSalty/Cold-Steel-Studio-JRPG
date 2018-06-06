ab0b336759f891fd69212b05e08eb05da9537240
TheDapperPenguin <bailleewilson@gmail.com>
2018-06-06 12:35:37 -0700

    Pickup script
    also with test assets

---
+++ JRPG/Assets/InventoryScript.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InventoryScript : MonoBehaviour
+{
+    public Inventory inventory;
+    public UINotificationManager notificationmanager;
+    public GameObject inter;
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.K))
+        {
+            InventoryInterface itemdetails = inter.GetComponent<InventoryInterface>();
+            int pOldCount = inventory.pInventory.Count;
+            int bOldCount = inventory.bInventory.Count;
+            inventory.pickUp(inter, Inventory.InventoryModes.Normal);
+            if (pOldCount < inventory.pInventory.Count || bOldCount < inventory.bInventory.Count)
+                notificationmanager.CreateNotification(itemdetails.itemName + " has been added to your inventory", null, 3);
+        }
+    }
+}
---
+++ JRPG/Assets/OnCollisionPickup.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OnCollisionPickup : MonoBehaviour {
+	// Use this for initialization
+	void Start () {
+     
+	}
+    private void OnCollisionEnter(Collision collision)
+    {
+        Debug.Log("collided");
+        Inventory inv = collision.gameObject.GetComponent<Inventory>();
+        if (inv != null) {
+            inv.pickUp(this.gameObject,Inventory.InventoryModes.Normal);
+        }
+    }
+
+}
